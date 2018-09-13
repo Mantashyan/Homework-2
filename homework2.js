6d52efb11079f7549cc522aa73ed78888ab57ef5
Mantashyan <43172814+Mantashyan@users.noreply.github.com>
2018-09-13 21:10:06 +0400

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,17 @@
+const firstname = "Mariam";
+const lastname = "Mantashyan";
+const fullname = "Mariam "+ "Mantashyan"
+const age = 18;
+
+if(age !== 18 && firstname === "Mariam") {
+  console.log("EY dude, erase your last point! You are under 18!!!");
+} else {
+  console.log("This list belongs to Mariam Mantashyan ^_^");
+}
+
+if(fullname=== "Mariam" - "Mantashyan") {
+	cosole.log("But don't be upset, we still love you");
+} else {
+	console.log("And she recommends all of you to stay organized and  do not procrastinate like she does");
+}
+
